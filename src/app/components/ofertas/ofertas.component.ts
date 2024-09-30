78e1a320fb05cd02794c444cdaa0dd4e7d768354
JavierSeronGuzman <seron.jsg@gmail.com>
2024-09-29 23:38:05 -0300

    Cambios de home formato mas chico

--- src/app/components/ofertas/ofertas.component.ts
+++ src/app/components/ofertas/ofertas.component.ts
@@ -8,7 +8,8 @@ import { CommonModule } from '@angular/common';
   selector: 'app-ofertas',
   standalone: true,
   imports: [RouterModule,CommonModule],
-  templateUrl: './ofertas.component.html'
+  templateUrl: './ofertas.component.html',
+  styleUrl: './ofertas.component.css'
 })
 export class OfertasComponent implements OnInit{
 
